2bb9b4090591b34f016096a76cc7ec03db1a9436
Min Phyoe Min Thu <dejavunerdprogrammer001@gmail.com>
2026-08-16 08:24:57 +0630

    [M] Add Doc

---
+++ Design Patterns/Behavioral Design Patterns/Template Method Pattern/main.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Design_Patterns.Behavioral_Design_Patterns.Template_Method_Pattern
+{
+    public class main
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
