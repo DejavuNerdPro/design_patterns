2032703b1753edd307c2404a0ab520cba12b479d
Min Phyoe Min Thu <dejavunerdprogrammer001@gmail.com>
2026-08-16 23:26:39 +0630

    [M] Observer Pattern

---
+++ Design Patterns/Behavioral Design Patterns/Observer Pattern/main.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Design_Patterns.Behavioral_Design_Patterns.Observer_Pattern
+{
+    public class main
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
