4195e5e713cd7e3407b77629d39c80d9c547d958
Min Phyoe Min Thu <dejavunerdprogrammer001@gmail.com>
2026-08-18 10:05:04 +0630

    [M] Proxy doc

---
+++ Design Patterns/Structural Design Patterns/Proxy Pattern/main.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Design_Patterns.Structural_Design_Patterns.Proxy_Pattern
+{
+    public class main
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
